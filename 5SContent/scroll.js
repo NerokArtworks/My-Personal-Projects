7b218bde5b6910a798e9bff2b66f6019871ef32d
NerokArtworks <nerokprodscontact@gmail.com>
2021-11-18 13:56:45 +0100

    5S Content - ParallaxScrolling in Work Section

--- 5SContent/scroll.js
+++ 5SContent/scroll.js
@@ -1,6 +1,7 @@
 var contador = 0;
 var hLine = document.getElementsByClassName("h-red-bar")[0];
 var videoPersp = document.getElementsByClassName("s-video1")[0];
+var galeryParallax = false;
 
 function contadorState() {
     if (window.scrollY < 550) {
@@ -9,6 +10,9 @@ function contadorState() {
     else if (contador > 0, window.scrollY > 200) {
         videoPersp.classList.add("s-video1-show");
         hLine.style.bottom = "0rem";
+    }
+    if (window.scrollY > window.innerHeight * 1.5) {
+        galeryParallax = true;
     }
 
 }
@@ -34,3 +38,40 @@ var observer = new IntersectionObserver(function(entries) {
 }, { threshold: [0, 0.5, 1] });
 
 observer.observe(document.querySelector("#h-trigger"));
+
+// Parallax
+
+    // Container de los elementos
+    const container = document.getElementsByClassName('s-home-work-section')[0];
+    console.log(container);
+
+    // Elementos
+    const element01 = document.getElementsByClassName('s-work__item01')[0];
+    const element02 = document.getElementsByClassName('s-work__item02')[0];
+    const element03 = document.getElementsByClassName('s-work__item03')[0];
+    const element04 = document.getElementsByClassName('s-work__item04')[0];
+    const element05 = document.getElementsByClassName('s-work__item05')[0];
+
+    const parallaxElements = [element01, element02, element03, element04, element05];
+    //const elementBot = element01.getAttribute(bottom);
+
+    // Window sizes
+    const sizeY = window.innerHeight;
+    const sizeX = window.innerWidth;
+
+document.addEventListener('scroll', e => {
+    console.log(sizeY);
+    console.log(window.scrollY);
+    if (galeryParallax == true) {
+        console.log("Parallax activado");
+        element01.style.top = (window.scrollY/2) * -0.2 + "px";
+        element02.style.top = (window.scrollY/2) * -0.9 + "px";
+        element03.style.top = (window.scrollY/2) * -0.8 + "px";
+        element04.style.top = (window.scrollY/2) *  -0.09 + "px";
+        element05.style.top = (window.scrollY/2) * -0.75 + "px";
+    }
+    else console.log("Parallax desactivado");
+    
+});
+
+
